736d7488080be24ca754f6c6245ac0155638cd4b
Ricardo Fontes de França <fontes.ricardo@hotmail.com>
2020-03-09 22:35:03 -0300

    BubbleSort

---
+++ AlgoritmosDeOrdenacao/AlgoritmosDeOrdenacao/BubbleSort.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AlgoritmosDeOrdenacao
+{
+    public class BubbleSort
+    {
+        public IEnumerable<int> OrdenarPrimeiraVersao(IList<int> numerosDesordenados)
+        {
+            var numerosOrdenados = numerosDesordenados.ToList();
+
+            var posicaoJaOrdenada = 0;
+            for (int i = numerosOrdenados.Count - 1; i > posicaoJaOrdenada; i--)
+            {
+                if (numerosOrdenados[i] < numerosOrdenados[i - 1])
+                {
+                    var posicaoA = numerosOrdenados[i];
+                    var posicaoB = numerosOrdenados[i - 1];
+
+                    numerosOrdenados[i] = posicaoB;
+                    numerosOrdenados[i - 1] = posicaoA;
+                }
+                if (i == posicaoJaOrdenada + 1)
+                {
+                    posicaoJaOrdenada++;
+                    i = numerosOrdenados.Count;
+                }
+            }
+
+            return numerosOrdenados;
+        }
+
+        public IEnumerable<int> OrdenarDoFimAoInicio(IList<int> numerosDesordenados)
+        {
+            var numerosOrdenados = numerosDesordenados.ToList();
+
+            var moverNumero = 0;
+            var descontarPosicao = 1;
+
+            for (int write = 0; write < numerosOrdenados.Count; write++)
+            {
+                for (int sort = numerosOrdenados.Count - 1; sort > descontarPosicao - 1; sort--)
+                {
+                    if (numerosOrdenados[sort] < numerosOrdenados[sort - 1])
+                    {
+                        moverNumero = numerosOrdenados[sort - 1];
+                        numerosOrdenados[sort - 1] = numerosOrdenados[sort];
+                        numerosOrdenados[sort] = moverNumero;
+                    }
+                }
+                descontarPosicao++;
+            }
+
+            return numerosOrdenados;
+        }
+
+        public IEnumerable<int> OrdenarDoInicioAoFim(IList<int> numerosDesordenados)
+        {
+            var numerosOrdenados = numerosDesordenados.ToList();
+
+            var moverNumero = 0;
+            var descontarPosicao = 1;
+
+            for (int write = 0; write < numerosOrdenados.Count; write++)
+            {
+                for (int sort = 0; sort < numerosOrdenados.Count - descontarPosicao; sort++)
+                {
+                    if (numerosOrdenados[sort] > numerosOrdenados[sort + 1])
+                    {
+                        moverNumero = numerosOrdenados[sort + 1];
+                        numerosOrdenados[sort + 1] = numerosOrdenados[sort];
+                        numerosOrdenados[sort] = moverNumero;
+                    }
+                }
+                descontarPosicao++;
+            }
+
+            return numerosOrdenados;
+        }
+    }
+}
---
+++ AlgoritmosDeOrdenacao/AlgoritmosDeOrdenacao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoritmosDeOrdenacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numerosDesordenados = new List<int> { 5, 9, 3, 1, 2, 8, 4, 7, 6 };
+
+            var numerosOrdenados1 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
+
+            var numerosOrdenados2 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
+
+            var numerosOrdenados3 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
+
+            Console.WriteLine("Lista ordenada 1");
+            foreach (var numero in numerosOrdenados1)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Lista ordenada 2");
+            foreach (var numero in numerosOrdenados2)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Lista ordenada 3");
+            foreach (var numero in numerosOrdenados3)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
