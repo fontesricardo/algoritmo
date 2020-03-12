3df760325ace4333c00b27ccba34b0cc08958e25
Ricardo Fontes de França <fontes.ricardo@hotmail.com>
2020-03-11 23:17:39 -0300

    Ordenação usando algoritmo selection sort

--- AlgoritmosDeOrdenacao/AlgoritmosDeOrdenacao/Program.cs
+++ AlgoritmosDeOrdenacao/AlgoritmosDeOrdenacao/Program.cs
@@ -7,33 +7,11 @@ namespace AlgoritmosDeOrdenacao
     {
         static void Main(string[] args)
         {
-            var numerosDesordenados = new List<int> { 5, 9, 3, 1, 2, 8, 4, 7, 6 };
+            new BubbleSort().Executar();
 
-            var numerosOrdenados1 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
+            new SelectionSort().Executar();
 
-            var numerosOrdenados2 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
-
-            var numerosOrdenados3 = new BubbleSort().OrdenarDoInicioAoFim(numerosDesordenados);
-
-            Console.WriteLine("Lista ordenada 1");
-            foreach (var numero in numerosOrdenados1)
-            {
-                Console.WriteLine(numero);
-            }
-
-            Console.WriteLine("Lista ordenada 2");
-            foreach (var numero in numerosOrdenados2)
-            {
-                Console.WriteLine(numero);
-            }
-
-            Console.WriteLine("Lista ordenada 3");
-            foreach (var numero in numerosOrdenados3)
-            {
-                Console.WriteLine(numero);
-            }
-
-            Console.ReadLine();
+            Console.Read();
         }
     }
 }
---
+++ AlgoritmosDeOrdenacao/AlgoritmosDeOrdenacao/SelectionSort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AlgoritmosDeOrdenacao
+{
+    public class SelectionSort
+    {
+        public void Executar()
+        {
+            var numerosDesordenados = new List<int> { 5, 9, 3, 1, 2, 8, 4, 7, 6 };
+
+            var selectionSort = new SelectionSort();
+
+            var numerosOrdenados1 = selectionSort.OrdenarPrimeiraVersao(numerosDesordenados);
+
+            var numerosOrdenados2 = selectionSort.Ordenar(numerosDesordenados);
+        }
+
+        private IEnumerable<int> OrdenarPrimeiraVersao(IEnumerable<int> numerosDesordenados)
+        {
+            var numerosOrdenados = numerosDesordenados.ToList();
+
+            int posicaoMenorNumero;
+            int trocarDePosicao;
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int write = 0; write < numerosOrdenados.Count; write++)
+            {
+                posicaoMenorNumero = write;
+                for (int sort = write + 1; sort < numerosOrdenados.Count; sort++)
+                {
+                    if (numerosOrdenados[posicaoMenorNumero] > numerosOrdenados[sort])
+                        posicaoMenorNumero = sort;
+                }
+                trocarDePosicao = numerosOrdenados[write];
+                numerosOrdenados[write] = numerosOrdenados[posicaoMenorNumero];
+                numerosOrdenados[posicaoMenorNumero] = trocarDePosicao;
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{nameof(SelectionSort)}.{nameof(OrdenarPrimeiraVersao)} => Fim {stopwatch.Elapsed}");
+
+            return numerosOrdenados;
+        }
+
+        private IEnumerable<int> Ordenar(IEnumerable<int> numerosDesordenados)
+        {
+            var numerosOrdenados = numerosDesordenados.ToList();
+
+            int totalNumeros = numerosOrdenados.Count();
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < totalNumeros - 1; i++)
+            {
+                int posicaoMenorNumero = i;
+                for (int j = i + 1; j < totalNumeros; j++)
+                    if (numerosOrdenados[j] < numerosOrdenados[posicaoMenorNumero])
+                        posicaoMenorNumero = j;
+
+                int temp = numerosOrdenados[posicaoMenorNumero];
+                numerosOrdenados[posicaoMenorNumero] = numerosOrdenados[i];
+                numerosOrdenados[i] = temp;
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"{nameof(SelectionSort)}.{nameof(Ordenar)} => Fim {stopwatch.Elapsed}");
+
+            return numerosOrdenados;
+        }
+    }
+}
